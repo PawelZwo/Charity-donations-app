5b76868432558131042ae933dd79bd572bcc7490
PawelZwo <pawel_zwolinski@icloud.com>
2023-07-06 14:24:52 +0200

    settings changed

--- base/settings.py
+++ base/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'charity',
+    'charity.apps.CharityConfig',
 ]
 
 MIDDLEWARE = [
